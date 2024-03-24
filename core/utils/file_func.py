6ca02c65a045b9de1704bc44d54909bae978f8a4
Axcent <97802272+Axcent-ape@users.noreply.github.com>
2024-03-24 21:27:47 +0200

    Add files via upload

---
+++ core/utils/file_func.py
@@ -0,0 +1,17 @@
+import random
+
+
+async def random_line(filepath: str, delete: bool = True):
+    with open(filepath, 'r') as file:
+        keys = file.readlines()
+
+    if not keys:
+        return False
+    random_key = random.choice(keys)
+    if delete:
+        keys.remove(random_key)
+
+        with open(filepath, 'w') as file:
+            file.writelines(keys)
+
+    return random_key.strip()
